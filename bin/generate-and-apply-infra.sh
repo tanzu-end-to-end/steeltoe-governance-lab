afdc0a791d8546aa1832141df51a12b784287cd2
Chuck D'Antonio <cdantonio@pivotal.io>
2020-12-21 10:45:00 -0500

    Deploys infrastructure to the correct namespace (#8)
    TL;DR
    -----
    
    Instructs `kapp` to deploy infrastructure to `musicstore` namespace
    
    Details
    -------
    
    Fixes an earlier update to use the `tanzu-kapp` namespace for the
    `kapp` metadata for the infrastructure services to make sure that
    the services are deployed into the `musicstore` namespace where
    the music store services and interface expect them to be.

--- bin/generate-and-apply-infra.sh
+++ bin/generate-and-apply-infra.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-ytt -f k8s/infra.yml -f secrets/lab.yml --ignore-unknown-comments | 
-    kapp deploy -n tanzu-kapp -a steeltoe-services -f - -y
+WORKLOAD_CLUSTER=$(yq r ${PARAMS_YAML} clusters.workload-cluster)
+
+PREVIOUS_CONTEXT=$(kubectl config current-context)
+kubectl config use-context "${WORKLOAD_CLUSTER}-admin@${WORKLOAD_CLUSTER}"
+ytt -f k8s/infra.yml -f secrets/lab.yml --ignore-unknown-comments |
+    kapp deploy -n tanzu-kapp -a steeltoe-services --into-ns musicstore -f - -y
+kubectl config use-context ${PREVIOUS_CONTEXT}
 
