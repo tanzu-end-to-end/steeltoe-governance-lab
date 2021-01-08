7d98c007a6600d79ca062ab4257ff61bf318ec36
Chuck D'Antonio <c_dantonio@acm.org>
2021-01-08 13:54:27 -0500

    Removes dead components (#12)
    TL;DR
    -----
    
    Deletes artifacts that aren't needed with the current lab setup
    
    Details
    -------
    
    As I documented the lab setup I realized that the best approach
    was to build on the Tanzu Governance Lab rather than to replicate
    steps in both places. That decision left a few artifacts in this
    repository that weren't needed any longer. This change removes the
    redundant artifacts.

--- bin/build-and-push-scanner-image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-REGISTRY=$(yq r $PARAMS_YAML common.harborDomain)
-docker build -t  dotnet-sonarscanner docker/dotnet-sonarscanner
-docker tag dotnet-sonarscanner:latest ${REGISTRY}/tools/dotnet-sonarscanner:latest
-docker push ${REGISTRY}/tools/dotnet-sonarscanner:latest
